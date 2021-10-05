332b2d52ff68af0e9b588acd055e3bd7dd7aa7e3
Vitalikkobinevych <90349901+Vitalikkobinevych@users.noreply.github.com>
2021-10-05 20:13:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,27 +12,27 @@ def check_rozmir():
         print("Number must be > 0 ")
         return check_rozmir()
     return n
-def input_matrix():
+def input_matrix(n):
     matrix = [[check_int()for y in range(n)]for x in range(n)]
     return matrix
+def print_matrix(matrix, n):
+    for im in range(n):
+        print(matrix[im])
 def search():
+    n = check_rozmir()
+    matrix = input_matrix(n)
     j = 0
     i = 0
+    print_matrix(matrix, n)
     while j < n:
         if int(matrix[j][j]) <= 0:
-            while i < n-1:
-                if matrix[j][i-1] < matrix[j][i+1]:
-                    min = matrix[j][i-1]
+            while i < n:
+                if matrix[j][i - 1] < matrix[j][i]:
+                    min = matrix[j][i - 1]
                 else:
-                    min = matrix[j][i+1]
+                    min = matrix[j][i]
                 i = i + 1
-            print(min)
-        else:
-            print("None")
+            i = 0
         j = j + 1
-    return 0
-n = check_rozmir()
-matrix = input_matrix()
-for im in range(n):
-    print(matrix[im])
+    print(min)
 search()
